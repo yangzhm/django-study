f6ef9953f77d3df9942e238ce86cf0c9bd59ec8e
yang zhenming <yangzhenming004@163.com>
2018-03-05 23:09:17 -0800

    add models

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
 ]
